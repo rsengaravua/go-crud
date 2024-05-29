5c27e3ca58dba6aa34679be592362f7773eddfc7
Rahul Singh Sengar <buddi@BUDDI-2.local>
2024-05-29 09:15:33 +0530

    removed unnecessary comments

--- pkg/common/models/book.go
+++ pkg/common/models/book.go
@@ -8,12 +8,3 @@ type Book struct {
 	Author      string `json:"author"`
 	Description string `json:"description"`
 }
-
-// func MigrateDB(db *gorm.DB) error {
-// 	// Auto Migrate will ONLY create tables, missing columns and missing indexes,
-// 	// and WON'T change existing column's type or delete unused columns to protect your data.
-// 	if err := db.AutoMigrate(&Book{}); err != nil {
-// 		return err
-// 	}
-// 	return nil
-// }
