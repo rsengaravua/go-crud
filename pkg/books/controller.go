7908ff0c246aa6ec802717c6f27ae3a9e1580256
Rahul Singh Sengar <buddi@BUDDI-2.local>
2024-05-31 15:31:50 +0530

    removed unnecessary comments

--- pkg/books/controller.go
+++ pkg/books/controller.go
@@ -19,6 +19,6 @@ func RegisterRoutes(router *gin.Engine, db *sql.DB) {
 	routes.GET("/AllBooks", h.GetBooks)
 	routes.GET("/book/:id", h.GetBook)
 	routes.POST("/book", h.AddBook)
-	routes.PUT("/book/:id", h.UpdateBook)    // Corrected route definition for updating a book
-	routes.DELETE("/book/:id", h.DeleteBook) // Corrected route definition for deleting a book
+	routes.PUT("/book/:id", h.UpdateBook)
+	routes.DELETE("/book/:id", h.DeleteBook)
 }
